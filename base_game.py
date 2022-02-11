41403b7c7586ff6c66bd298e6a96e47833b4ee96
Stanoe3 <stanbrown1605@gmail.com>
2022-02-11 13:35:19 +0000

    Implemented a minimax algorithm

--- base_game.py
+++ base_game.py
@@ -82,11 +82,14 @@ class TicTacToe():
         if (self.check_diagonal_victory()
             or self.check_horizontal_victory()
             or self.check_vertical_victory()):
-            return self.check_diagonal_victory()
+            return (self.check_diagonal_victory()
+                or self.check_horizontal_victory()
+                or self.check_vertical_victory())
         return False
 
     def play(self):
         while not self.check_victory():
+            print(self.check_victory())
             current_player = self.players[self.turn_count % 2]
             if current_player == "Player":
                 self.get_player_turn()
@@ -96,8 +99,6 @@ class TicTacToe():
             print(self.grid)
         winning_symbol = self.check_victory()
         print(f"Congratulations {self.symbol_dict[winning_symbol]}! You win!")
-
-
 
     def get_player_turn(self):
         valid = False
@@ -109,15 +110,69 @@ class TicTacToe():
 
 
     def get_AI_turn(self):
-        valid = False
-        while not valid:
-            row = random.randint(0,2)
-            column = random.randint(0,2)
-            valid = self.check_valid(row, column)
-        self.fill_cell(self.AI_symbol, row, column)
+        move = self.find_best_move(self.grid)
+        self.fill_cell(self.AI_symbol, move[0], move[1])
 
+    def moves_left(self):
+        for row in self.grid:
+            for cell in row:
+                if cell == None:
+                    return True
+        return False
 
+    def minimax(self, board, depth, is_max):
+        if self.check_victory() == self.player_symbol:
+            score = -10
+            return score
+        if self.check_victory() == self.AI_symbol:
+            score = 10
+            return score
+        else:
+            score = 0
 
+        if not self.moves_left():
+            return 0
+
+        if is_max:
+            best_value = -100
+            for row_num in range(self.size):
+                for col_num in range(self.size):
+                    if board[row_num][col_num] == None:
+                        board[row_num][col_num] = self.AI_symbol
+
+                        best_value = max(best_value, self.minimax(board,
+                                                                  depth+1,
+                                                                  not is_max))
+                        board[row_num][col_num] = None
+            return best_value
+        else:
+            best_value = 1000
+
+            for row_num in range(self.size):
+                for col_num in range(self.size):
+                    if board[row_num][col_num] == None:
+                        board[row_num][col_num] = self.player_symbol
+
+                        best_value = min(best_value, self.minimax(board,
+                                                                  depth+1,
+                                                                  not is_max))
+                        board[row_num][col_num] = None
+            return best_value
+
+    def find_best_move(self, board):
+        best_value = -1000
+        best_move = (-1, -1)
+
+        for row_num in range(self.size):
+            for col_num in range(self.size):
+                if board[row_num][col_num] == None:
+                    board[row_num][col_num] = self.AI_symbol
+                    move_value = self.minimax(board, 0, False)
+                    board[row_num][col_num] = None
+                    if move_value > best_value:
+                        best_move = (row_num, col_num)
+                        best_value = move_value
+        return best_move
 
 
 if __name__ == '__main__':
