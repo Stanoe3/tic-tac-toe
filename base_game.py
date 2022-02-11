7a9500cca7e9796eaa3150117623da2327ec33fa
Stanoe3 <stanbrown1605@gmail.com>
2022-02-11 12:59:05 +0000

    Basic playable game (Random AI)

--- base_game.py
+++ base_game.py
@@ -1,19 +1,32 @@
+import random
+
 class TicTacToe():
     def __init__(self):
         self.size = 3
+        self.players = ["Player", "Computer"]
         self.grid = [[None for y in range(self.size)]for x in range(self.size)]
         self.turn_count = 0
+        self.player_symbol = input("Please enter your symbol: ")
+        if self.player_symbol.lower() == "o":
+            self.AI_symbol = "X"
+        else:
+            self.AI_symbol = "O"
+        self.symbol_dict = {
+            self.AI_symbol: "Computer",
+            self.player_symbol: "Player"
+        }
+
+        self.play()
 
     def fill_cell(self, player_symbol, row, column):
         self.grid[row][column] = player_symbol
 
-    def checkValid(self, row, column):
+    def check_valid(self, row, column):
         if row >= self.size or column >= self.size:
             return False
-        if self.grid[row][column]:
+        if self.grid[row][column] != None:
             return False
         return True
-
 
     def check_horizontal_victory(self):
         for row in self.grid:
@@ -25,20 +38,20 @@ class TicTacToe():
             if empty:
                 continue
             if max(row) == min(row):
-                return True
+                return row[0]
         return False
 
     def check_vertical_victory(self):
-        for row in [*zip(*self.grid)]:
+        for col in [*zip(*self.grid)]:
             empty = False
-            for cell in row:
+            for cell in col:
                 if not cell:
                     empty = True
                     break
             if empty:
                 continue
-            if max(row) == min(row):
-                return True
+            if max(col) == min(col):
+                return col[0]
         return False
 
     def check_diagonal_victory(self):
@@ -59,18 +72,49 @@ class TicTacToe():
             return False
         if not left_diag_empty:
             if min(left_diag) == max(left_diag):
-                return True
+                return left_diag[0]
         if not right_diag_empty:
             if min(right_diag) == max(right_diag):
-                return True
+                return right_diag[0]
         return False
 
     def check_victory(self):
         if (self.check_diagonal_victory()
             or self.check_horizontal_victory()
             or self.check_vertical_victory()):
-            return True
+            return self.check_diagonal_victory()
         return False
+
+    def play(self):
+        while not self.check_victory():
+            current_player = self.players[self.turn_count % 2]
+            if current_player == "Player":
+                self.get_player_turn()
+            else:
+                self.get_AI_turn()
+            self.turn_count += 1
+            print(self.grid)
+        winning_symbol = self.check_victory()
+        print(f"Congratulations {self.symbol_dict[winning_symbol]}! You win!")
+
+
+
+    def get_player_turn(self):
+        valid = False
+        while not valid:
+            row = -1 +int(input("Please enter the row: "))
+            column = -1 + int(input("Please enter the column: "))
+            valid = self.check_valid(row, column)
+        self.fill_cell(self.player_symbol, row, column)
+
+
+    def get_AI_turn(self):
+        valid = False
+        while not valid:
+            row = random.randint(0,2)
+            column = random.randint(0,2)
+            valid = self.check_valid(row, column)
+        self.fill_cell(self.AI_symbol, row, column)
 
 
 
@@ -78,14 +122,5 @@ class TicTacToe():
 
 if __name__ == '__main__':
     tic = TicTacToe()
-    tic.fill_cell("x",1,0)
-    tic.fill_cell("x",1,1)
-    tic.fill_cell("x", 0, 2)
-    #tic.fill_cell("x",1,2)
-    tic.fill_cell("x",0,1)
-    tic.fill_cell("x",2,1)
-    tic.fill_cell("x",2,0)
-    print(tic.grid)
-    print(tic.check_victory())
 
 
